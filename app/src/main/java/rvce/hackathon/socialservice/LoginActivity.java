0ee7013407edef68d63984b8e457a63a7e6d0625
niteeshhegde <niteeshhegde@gmail.com>
2017-03-24 23:51:26 +0530

    niteesh demo card add and view

--- app/src/main/java/rvce/hackathon/socialservice/LoginActivity.java
+++ app/src/main/java/rvce/hackathon/socialservice/LoginActivity.java
@@ -1,5 +1,6 @@
 package rvce.hackathon.socialservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        startActivity(new Intent(LoginActivity.this,EventsActivity.class));
     }
 }
